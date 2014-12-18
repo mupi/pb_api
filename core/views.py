4a6f4cc5f36c27be6d1d12e179e6ba366e3b9a81
eliassouza <eliaslsouza@gmail.com>
2014-12-18 14:30:35 -0200

    Associado API

--- core/views.py
+++ core/views.py
@@ -3,8 +3,45 @@ from rest_framework import viewsets, generics, status
 from rest_framework.response import Response
 from rest_framework.decorators import detail_route, api_view
 
-from .serializers import UserSerializer, ProgramaSerializer
-from .models import Programa
+from .serializers import AssociadoSerializer, ProgramaSerializer
+from .models import Associado, Programa
+
+class AssociadoViewSet(viewsets.ModelViewSet):
+
+    queryset = Associado.objects.all()
+    serializer_class = AssociadoSerializer
+
+    def list(self, request):
+        queryset = Associado.objects.all()
+        serializer = AssociadoSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Associado.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = AssociadoSerializer(user)
+        return Response(serializer.data)
+
+    def create(self, request):
+        if request.method == 'POST':
+            serializer = AssociadoSerializer(data=request.DATA)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        if request.method == 'PUT':
+            serializer = AssociadoSerializer(request.user, data=request.DATA, partial=True)
+        return super(AssociadoViewSet, self).update(request)
+
+    def destroy(self, request, pk=None):
+        if request.method == 'DELETE':
+            serializer = AssociadoSerializer(data=request.DATA)
+
+        return super(AssociadoViewSet, self).destroy(request)
 
 class ProgramaViewSet(viewsets.ModelViewSet):
 
--- pbapi/urls.py
+++ pbapi/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from core.views import ProgramaViewSet
+from core.views import AssociadoViewSet, ProgramaViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
+router.register(r'associados', AssociadoViewSet, 'AssociadosList')
 router.register(r'programas', ProgramaViewSet, 'ProgramasList')
 urlpatterns = router.urls
 
